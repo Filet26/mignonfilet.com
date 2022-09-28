b53563899f8e590ea59f24b5b429a6e836a900a3
Desmond Ho <desmondh268@gmail.com>
2022-09-28 12:40:40 -0700

    added meat adjustments

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,1 +1,21 @@
 // Main Script
+
+// Button for meat
+const grow_btn = document.getElementById("meatbuttongrow");
+const shrink_btn = document.getElementById("meatbuttonsmall");
+
+const meatimg = document.getElementById("meatmain");
+let size = 100;
+grow_btn.addEventListener("click", function onClick() {
+  if (size < 400) {
+    size += 20;
+    meatimg.style.width = `${size}px`;
+  }
+});
+
+shrink_btn.addEventListener("click", function onClick() {
+  if (size > 100) {
+    size -= 20;
+    meatimg.style.width = `${size}px`;
+  }
+});
